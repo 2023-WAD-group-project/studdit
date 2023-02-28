732c52d0ff6c59b505da14ef32dcc9121d80385a
Andrei Boghean <andreiboghean00@gmail.com>
2023-02-28 13:36:25 +0000

    switch pages to template system

--- WADproj_studdit/studdit/views.py
+++ WADproj_studdit/studdit/views.py
@@ -1,20 +1,32 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("homepage")
+    context_dict = {}
+    return render(request, "home.html", context=context_dict)
 
 def course(request):
+    # note that this view wont be fully completed until we set up the database,
+    # so worry about the other views for now
     raise Exception("NOT IMPLEMENTED")
-    return HttpResponse("course page")
+
+    context_dict = {}
+    return render(request, "course.html", context=context_dict)
 
 def post(request):
+    # note that this view wont be fully completed until we set up the database,
+    # so worry about the other views for now
     raise Exception("NOT IMPLEMENTED")
-    return HttpResponse("post page")
+
+    context_dict = {}
+    return render(request, "post.html", context=context_dict)
 
 def login(request):
-    return HttpResponse("login")
+    context_dict = {}
+    return render(request, "login.html", context=context_dict)
 
 def profile(request):
-    return HttpResponse("profile")
+    context_dict = {}
+    return render(request, "profile.html", context=context_dict)
