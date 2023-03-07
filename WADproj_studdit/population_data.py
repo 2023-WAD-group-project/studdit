a375184c1ef59c3ccbe49824e3e1a3613e25018b
Andrei Boghean <andreiboghean00@gmail.com>
2023-03-07 13:55:28 +0000

    more courses in popluation script

--- WADproj_studdit/population_data.py
+++ WADproj_studdit/population_data.py
@@ -359,15 +359,582 @@ comp_y2 = [
         "title": "Mentoring for Graduate Apprentices COMPSCI 2034 - Mentoring for Graduate Apprentices",
         "posts": [
             {
-                "title": "jjj",
+                "title": "",
                 "filename": "",
                 "description": ""
-                }
+            }
         ]
     }
 ]
 
-courses = comp_y1 + comp_y2
+eng_y1 = [
+{
+    "code": "ENG1002",
+    "title": "Aerospace Engineering 1 ENG 1002 - Aerospace Engineering 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1003",
+    "title": "Analogue Electronics 1 ENG 1003 - Analogue Electronics 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1008",
+    "title": "Architecture C1 (Glasgow School of Art) ENG 1008 - Architecture C1 (Glasgow School of Art)",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1015",
+    "title": "Design and Manufacture 1 ENG 1015 - Design and Manufacture 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1016",
+    "title": "Mechanical Design 1 ENG 1016 - Mechanical Design 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1021",
+    "title": "Electronic Engineering 1X ENG 1021 - Electronic Engineering 1X",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1022",
+    "title": "Electronic Engineering 1Y ENG 1022 - Electronic Engineering 1Y",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1026",
+    "title": "Engineering Skills 1 ENG 1026 - Engineering Skills 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1027",
+    "title": "Environmental Engineering 1 ENG 1027 - Environmental Engineering 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1031",
+    "title": "Introduction To Biomedical Engineering 1 ENG 1031 - Introduction To Biomedical Engineering 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1033",
+    "title": "Materials 1 ENG 1033 - Materials 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1061",
+    "title": "Civil Engineering 1 ENG 1061 - Civil Engineering 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1062",
+    "title": "Dynamics 1 ENG 1062 - Dynamics 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1063",
+    "title": "Engineering Mathematics 1 ENG 1063 - Engineering Mathematics 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1064",
+    "title": "Microelectronic Systems 1 ENG 1064 - Microelectronic Systems 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1065",
+    "title": "Statics 1 ENG 1065 - Statics 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1066",
+    "title": "Thermodynamics 1 ENG 1066 - Thermodynamics 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG1071",
+    "title": "Biomolecular Processes in Bioengineering 1 ENG 1071 - Biomolecular Processes in Bioengineering 1",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+}
+]
+
+eng_y2 = [
+{
+    "code": "ENG2002",
+    "title": "Aerospace Design Project 2 ENG 2002 - Aerospace Design Project 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2004",
+    "title": "Analogue Electronics 2 ENG 2004 - Analogue Electronics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2008",
+    "title": "Architecture C2 (Glasgow School of Art) ENG 2008 - Architecture C2 (Glasgow School of Art)",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2011",
+    "title": "Biomaterials 2 ENG 2011 - Biomaterials 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2012",
+    "title": "Biomedical Engineering Skills 2 ENG 2012 - Biomedical Engineering Skills 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2015",
+    "title": "Design and Manufacture 2 ENG 2015 - Design and Manufacture 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2016",
+    "title": "Mechanical Design 2 ENG 2016 - Mechanical Design 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2020",
+    "title": "Digital Electronics 2 ENG 2020 - Digital Electronics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2023",
+    "title": "Electrical Circuits 2 ENG 2023 - Electrical Circuits 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2025",
+    "title": "Electronic Design Project 2 ENG 2025 - Electronic Design Project 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2026",
+    "title": "Electronic Devices 2 ENG 2026 - Electronic Devices 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2029",
+    "title": "Embedded Processors 2 ENG 2029 - Embedded Processors 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2031",
+    "title": "Engineering Electromagnetics 2 ENG 2031 - Engineering Electromagnetics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2037",
+    "title": "Introduction to Aerodynamics 2 ENG 2037 - Introduction to Aerodynamics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2039",
+    "title": "Materials 2 ENG 2039 - Materials 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2042",
+    "title": "Mathematics AE2X ENG 2042 - Mathematics AE2X",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2045",
+    "title": "Power Electronics 2 ENG 2045 - Power Electronics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2047",
+    "title": "Soil Mechanics 2 ENG 2047 - Soil Mechanics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2048",
+    "title": "Structural Design 2 ENG 2048 - Structural Design 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2053",
+    "title": "Thermodynamics 2 ENG 2053 - Thermodynamics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2071",
+    "title": "Structural Design Project 2 ENG 2071 - Structural Design Project 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2077",
+    "title": "Engineering Skills 2 ENG 2077 - Engineering Skills 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2078",
+    "title": "Environmental Processes 2 ENG 2078 - Environmental Processes 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2079",
+    "title": "Civil Engineering Skills 2 ENG 2079 - Civil Engineering Skills 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2080",
+    "title": "Geology and Surveying for Civil Engineers 2 ENG 2080 - Geology and Surveying for Civil Engineers 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2081",
+    "title": "Mechanics of Structures 2A ENG 2081 - Mechanics of Structures 2A",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2082",
+    "title": "Mechanics of Structures 2B ENG 2082 - Mechanics of Structures 2B",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2083",
+    "title": "Introductory Programming 2 ENG 2083 - Introductory Programming 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2084",
+    "title": "Dynamics 2 ENG 2084 - Dynamics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2085",
+    "title": "Fluid Mechanics 2 ENG 2085 - Fluid Mechanics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2086",
+    "title": "Engineering Mathematics 2 ENG 2086 - Engineering Mathematics 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2087",
+    "title": "Engineering in Biology 2 ENG 2087 - Engineering in Biology 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+{
+    "code": "ENG2088",
+    "title": "Optical Engineering 2 ENG 2088 - Optical Engineering 2",
+    "posts": [
+        {
+            "title": "",
+            "filename": "",
+            "description": ""
+        }
+    ]
+},
+]
+
+courses = comp_y1 + comp_y2 + eng_y1 + eng_y2 #+ phys_y1 + phys_y2
 
 for i in range(len(courses)):
     if courses[i]["posts"][0]["filename"] == "":
