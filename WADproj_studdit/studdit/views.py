9d6b18370efe37ecd30afa58ea3206716b8b613c
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2023-03-08 22:31:02 +0000

    fix code duplication

--- WADproj_studdit/studdit/views.py
+++ WADproj_studdit/studdit/views.py
@@ -14,28 +14,6 @@ def test(request):
     context_dict = {}
     return render(request, "test.html", context=context_dict)
 
-
-def course(request):
-    # note that this view wont be fully completed until we set up the database,
-    # so worry about the other views for now
-    #raise Exception("NOT IMPLEMENTED")
-
-    context_dict = {}
-    return render(request, "course.html", context=context_dict)
-
-def post(request):
-    context_dict = {}
-    return render(request, "post.html", context=context_dict)
-
-def login(request):
-    context_dict = {}
-    return render(request, "login.html", context=context_dict)
-
-def profile(request):
-    context_dict = {}
-    return render(request, "profile.html", context=context_dict)
-
-
 def show_course(request, course_name_slug):
     context_dict = {}
 
