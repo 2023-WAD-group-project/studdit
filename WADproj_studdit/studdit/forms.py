37e605a0feb73d0d4dcdfece528c163791885b01
Kyle Mcilwraith <kylemcilwraith@Kyles-MacBook-Pro-2.local>
2023-03-09 00:27:43 +0000

    updated postForm

--- WADproj_studdit/studdit/forms.py
+++ WADproj_studdit/studdit/forms.py
@@ -1,6 +1,8 @@
 from django import forms 
 from studdit.models import Post ,Course, Student
 from django.contrib.auth.models import User
+from population_data import courses
+from django.utils import timezone
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -9,5 +11,25 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'password', 'email',)
                                
-##need to add postForm
-##agreed to leaving out courseForm as courses can be added manually (by admin)
+
+# agreed to leaving out courseForm as courses can be added manually (by admin)
+class postForm(forms.ModelForm):
+
+    # i also feel like there should be a something declaring what
+    # student the post belongs to
+    
+    # i think this should work will need to tried
+    course = forms.CharField(max_length=32, widget=forms.Select(choices=courses))
+    
+    title = forms.CharField(max_length=32, unique=True)
+    description = forms.CharField(max_length=1024*10)
+    filename = forms.CharField(max_length=128, widget=forms.HiddenInput())
+    
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    timeOfCreation = timezone.now()
+    
+    # wasnt sure what other fields to include, dont know if 
+    # necessary to include upvoted_by or downvoted_by and not 
+
+    class Meta:
+        model = Post
