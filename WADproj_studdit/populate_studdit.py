837add447f6adebe2056a1e843db53f7c5802689
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2023-03-14 18:07:04 +0000

    tweaks to created users' credentials

--- WADproj_studdit/populate_studdit.py
+++ WADproj_studdit/populate_studdit.py
@@ -71,14 +71,14 @@ def populate():
 
     authors = []
 
-    user = User(username="Eric", email="2645295E@gla.ac.uk", password="EricPass?")
+    user = User(username="Eric", email="2645295E@student.gla.ac.uk", password="EricPass?")
     user.save()
     user.set_password(user.password)
     user.save()
     author = Student(user=user)
     author.save()
     authors.append(author)
-    user = User(username="John", email="2645295J@gla.ac.uk", password="JohnPass?")
+    user = User(username="John", email="2645295J@student.gla.ac.uk", password="JohnPass?")
     user.save()
     user.set_password(user.password)
     user.save()
