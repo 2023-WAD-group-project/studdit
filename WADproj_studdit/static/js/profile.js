71799e066c459938217ddc951dbcf5ce25005638
Andrei Boghean <andreiboghean00@gmail.com>
2023-03-22 12:51:39 +0000

    abstract profile css and fix jquery loading

---
+++ WADproj_studdit/static/js/profile.js
@@ -0,0 +1,16 @@
+$("#posts_div").ready(function() {
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("posts_div").innerHTML = this.responseText;
+        console.log(this.responseText)
+    }
+};
+
+var template_data = document.getElementById("template_data")
+var get_posts_url = template_data.getAttribute("data-get_posts_url")
+var user_username = template_data.getAttribute("data-user_username")
+xhttp.open("GET", get_posts_url + "?format=xml&student=" + user_username + "&xml_fields[]=votes&xml_fields[]=delete", true);
+xhttp.send();
+});
